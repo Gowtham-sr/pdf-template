c2a1290ce691199e28723072bf220e82023e205f
Gowtham-sr <gowtham.s1505@gmail.com>
2023-04-22 16:33:02 +0530

    Exercise: Add lines

--- main.py
+++ main.py
@@ -15,22 +15,26 @@ for index, row in df.iterrows():
     # RGB colours. You can assign it from 0-254.
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     # Cell's width, height(recommended font size), align left side, ln = break line
-    pdf.line(10, 21, 200, 21)
-    # x1, y1, x2, y2 co-ordinates in mm, coz you assigned it in mm.
+    for y in range(30, 298, 10):
+        pdf.line(10, y, 200, y)
+        # x1, y1, x2, y2 co-ordinates in mm, coz you assigned it in mm.
 
     # Set the footer
-    pdf.ln(265)
+    pdf.ln(267)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-    # range represents a list. Suppose range(3) represents [0, 1, 2] means i -> 0, 1, 2.
-    # Why -1 means coz already 1 page is created in parent for loop.
+        # range represents a list. Suppose range(3) represents [0, 1, 2] means i -> 0, 1, 2.
+        # Why -1 means coz already 1 page is created in parent for loop.
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
         # Set the footer
-        pdf.ln(277)
+        pdf.ln(279)
+        # 267 + 12(Cell height) = 279
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")

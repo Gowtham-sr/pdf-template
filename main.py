18a69bbe7069dd4de73e795c02abcdf68a1f6d7c
Gowtham-sr <gowtham.s1505@gmail.com>
2023-04-22 13:48:52 +0530

    Created multiple pages for each type

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     # RGB colours. You can assign it from 0-254.
@@ -15,4 +17,8 @@ for index, row in df.iterrows():
     pdf.line(10, 21, 200, 21)
     # x1, y1, x2, y2 co-ordinates in mm, coz you assigned it in mm.
 
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+    # range represents a list. Suppose range(3) represents [0, 1, 2] means i -> 0, 1, 2.
+    # Why -1 means coz already 1 page is created in parent for loop.
 pdf.output("output.pdf")
